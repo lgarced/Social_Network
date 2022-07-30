7423ee3079e1d4a84bd70806242fc6a27f44ea9e
Liberato Garced <93692943+lgarced@users.noreply.github.com>
2022-07-30 08:56:30 -0700

    Fixed some issues still broken

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,6 @@
 const router = require("express").Router();
 const userRoute = require("./api/users_route");
-const thoughtRoute = require("./api/thoughts_route");
+const thoughtRoute = require("./api/thought_route");
 
 router.use("/users", userRoute);
 router.use("/thoughts", thoughtRoute);

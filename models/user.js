b435b32ad5896dd29181002ac5b1bbc57e30a9f6
Liberato Garced <93692943+lgarced@users.noreply.github.com>
2022-07-30 05:59:47 -0700

    Changes on styles

---
+++ models/user.js
@@ -0,0 +1,34 @@
+const { Schema, model } = require("mongoose");
+//const moment = require('moment');
+const opt = { toJSON: { virtuals: true, getters: true, id: false } };
+
+//const Thought = require('../models/thought');
+
+const userSchema = new Schema(
+  {
+    username: {
+      type: String,
+      unique: true,
+      required: true,
+      trimmed: true,
+    },
+    email: {
+      type: String,
+      unique: true,
+      required: true,
+      match: [/^([a-z0-9_\.-]+)@([\da-z\.-]+)\.([a-z\.]{2,6})$/],
+    },
+    // one to many relationship
+    thoughts: [{ type: Schema.Types.ObjectId, ref: "Thought" }],
+    // one to many relationship
+    friends: [{ type: Schema.Types.ObjectId, ref: "User" }],
+  },
+  opt
+);
+const User = model("User", userSchema);
+// creates virtual to retrieve length of users friends
+userSchema.virtual("friendCount").get(function () {
+  return this.friends.length;
+});
+
+module.exports = User;
